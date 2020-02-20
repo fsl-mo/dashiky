c4d469ef0ab3161c6c4ce1ec1e7772a665839ec2
Faisal Mohamoud <faisal.mohamoud89@gmail.com>
2020-02-20 15:51:49 +0100

    Improve homepage
    Improve Product-Groups grid-view.
    Change palete colors.

--- src/components/product-groups/product-groups.component.jsx
+++ src/components/product-groups/product-groups.component.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { SHOP_DATA } from '../../data/data';
+
 import './product-groups.styles.scss';
 
-const ProductGroups = ({ data }) => {
-  if (!data) return null;
-  return (
-    <ul className="product-groups">
-      {data.map(item => (
-        <li key={item.id} className="product-group-item">
-          <Link to={`/collections/${item.id}`}>
-            <img src={`${item.imgUrl}`} alt={item.title} className="image" />
+const ProductGroups = () => (
+  <ul className="product-groups">
+    {SHOP_DATA.map(item => (
+      <li
+        key={item.id}
+        className={`product-group-item product-group-item-${item.id}`}
+      >
+        <Link to={`/collections/${item.routeName}`}>
+          <div
+            className="product-group-item-bg"
+            style={{ backgroundImage: `url(${item.imageUrl})` }}
+          />
+          <div className="product-group-item-cta">
             <h3 className="title">{item.title}</h3>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-};
+          </div>
+        </Link>
+      </li>
+    ))}
+  </ul>
+);
 
 export default ProductGroups;
