7f3fb5c0b24f571918443487fa8567513d4c4926
Faisal Mohamoud <faisal.mohamoud89@gmail.com>
2020-02-20 15:51:49 +0100

    í² Improve homepage
    Improve Product-Groups grid-view.
    Change palete colors.

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import ProductGroupsData from '../../mock/mock-data';
-
 import ProductGroups from '../../components/product-groups/product-groups.component';
 
 import './home.styles.scss';
 
 const Home = () => (
   <div className="home-page">
-    <ProductGroups data={ProductGroupsData} />
+    <section className="section-product-groups">
+      <ProductGroups />
+    </section>
   </div>
 );
 
