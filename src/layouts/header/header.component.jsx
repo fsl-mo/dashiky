f49b59b3cdffb412d7e9bfb26b5a1b53eb7a774e
Faisal Mohamoud <faisal.mohamoud89@gmail.com>
2020-04-13 23:35:58 +0200

    :lipstick: Update Header links and improve styles for header and footer

--- src/layouts/header/header.component.jsx
+++ src/layouts/header/header.component.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import Logo from '../../components/logo/logo.component';
 
 import './header.styles.scss';
 
-const Header = () => (
-  <header>
-    <Link to="/" className="app-logo">
-      <Logo variant="dark" />
-    </Link>
-    <ul className="nav-menu">
-      <li className="nav-item">
-        <Link to="#cart-menu" className="nav-link">
-          Cart
+const Header = () => {
+  const { pathname } = useHistory().location;
+  const setActiveClass = path => (pathname === path ? 'active' : '');
+  return (
+    <header>
+      <Link to="/" className="app-logo">
+        <Logo variant="dark" />
+      </Link>
+      <nav className="nav-links">
+        <Link to="/shop" className={`nav-link ${setActiveClass('/shop')}`}>
+          Shop
         </Link>
-      </li>
-    </ul>
-  </header>
-);
+
+        <Link to="/about" className={`nav-link ${setActiveClass('/about')}`}>
+          About
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
