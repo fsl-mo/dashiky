3372e774e998c7bacaede6e4a902776717d523a2
Faisal Mohamoud <faisal.mohamoud89@gmail.com>
2020-05-04 13:10:57 +0200

    🥅 Add Error-page component and render error message.

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,23 @@ import Footer from './layouts/footer/footer.component';
 import Home from './pages/home/home.component';
 import Shop from './pages/shop/shop.component';
 import About from './pages/about/about.component';
+import ErrorPage from './pages/error-page/error-page.component';
 
 const App = () => (
   <div className="app-wrapper">
     <Header />
     <main role="main">
       <Switch>
-        <Route exact path="/" component={Home} />
         <Route path="/shop" component={Shop} />
         <Route exact path="/about" component={About} />
-        {/* TODO: create 404 page and redirect broken links. */}
-        <Redirect to="/" />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/error-page" component={ErrorPage} />
+        <Redirect
+          to={{
+            pathname: '/error-page',
+            state: { error: { status: 404 } },
+          }}
+        />
       </Switch>
     </main>
     <Footer />
---
+++ client/src/pages/error-page/error-page.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
+
+import './error-page.styles.scss';
+
+const ErrorPage = () => {
+  const { state } = useLocation();
+  const error = state && state.error ? state.error : null;
+  const referrer = state && state.referrer ? state.referrer : '/';
+
+  return (
+    <div className="error-page">
+      <div className="content-wrapper container">
+        <div className="content">
+          <span className="emoji" role="img" aria-label="error">
+            😟
+          </span>
+          {error && <span className="subHeading">ERROR {error.status}</span>}
+          <h2 className="heading">
+            ooops, Something <br />
+            went wrong
+          </h2>
+
+          <Link to={referrer} className=" btn btn--primary">
+            Go Back
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
