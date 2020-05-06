6f346499fe52f5431dc4bded98ad228409163ae0
Faisal Mohamoud <faisal.mohamoud89@gmail.com>
2020-05-06 14:14:06 +0200

    :lipstick: Add Signin Component and styles

--- client/src/pages/signIn-and-signUp/signIn-and-SignUp.component.jsx
+++ client/src/pages/signIn-and-signUp/signIn-and-SignUp.component.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './signin-and-signup.styles.scss';
+import SigninForm from '../../components/signin-form/signin-form.component';
 
 const SigninAndSignup = () => {
   const [activeTab, setActiveTap] = useState(0);
@@ -17,50 +18,25 @@ const SigninAndSignup = () => {
         />
         <div className="card-content">
           {activeTab === 0 ? (
-            <div className="signIn-wrap">
-              <div className="heading">
-                <h2 className="heading-title">Sign in</h2>
-                <span className="heading-action">
-                  Not a member?
-                  <Link to="#" onClick={() => setActiveTap(1)}>
-                    Register
-                  </Link>
-                </span>
+            <>
+              <SigninForm />
+              <div className="card-action">
+                Not a member?
+                <Link to="#" onClick={() => setActiveTap(1)}>
+                  Register
+                </Link>
               </div>
-              <div>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio
-                in, cum voluptate eum veritatis ullam, autem, ab nihil
-                consequuntur soluta commodi aspernatur explicabo voluptatum
-                debitis obcaecati asperiores! Corrupti voluptatibus deleniti
-                possimus maxime. Quae dolorum vel nisi nemo hic ducimus animi ut
-                dolores, porro obcaecati asperiores illo nihil odit fugiat ab?
-                Inventore exercitationem corporis aperiam adipisci quisquam
-                optio ut enim sint incidunt tempore, in autem deleniti porro est
-                natus nesciunt, vel quos ex fugiat voluptates velit suscipit
-                omnis. Sunt aspernatur et placeat vitae illo repudiandae dolor
-                cum quasi error, dolores asperiores non voluptas nesciunt
-                obcaecati a facilis, quas reprehenderit ipsum sequi!
-              </div>
-            </div>
+            </>
           ) : (
-            <div className="signUp-wrap">
-              <div className="heading">
-                <h2 className="heading-title">Register</h2>
-                <span className="heading-action">
-                  Already registered?
-                  <Link to="#" onClick={() => setActiveTap(0)}>
-                    Log in
-                  </Link>
-                </span>
+            <>
+              <h2>Sign up</h2>
+              <div className="card-action">
+                Already registered?
+                <Link to="#" onClick={() => setActiveTap(0)}>
+                  Log in
+                </Link>
               </div>
-              <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                rerum ex facilis blanditiis aut. Magnam, voluptatem? Voluptates,
-                totam. Iure debitis quos blanditiis ab nesciunt neque? Nostrum
-                cumque eum earum, at, voluptatum, expedita nulla itaque dolor
-                eveniet quasi soluta ipsa eos!
-              </div>
-            </div>
+            </>
           )}
         </div>
       </div>
